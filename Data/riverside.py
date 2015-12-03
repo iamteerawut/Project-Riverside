74baf64c17ce79b49620e99d79bd6b789936a828
jappmarin <ja.parima2009@gmail.com>
2015-12-04 00:43:01 +0700

    Revert "Data 85%"
    This reverts commit a420d34ae5192fc8d65d071e588c9f20b862be3f.

--- Data/riverside.py
+++ Data/riverside.py
@@ -9,8 +9,7 @@ def project_um():
         value4 = []
         value5 = []
         for i in test:
-            data = i[0].split(';')
-            print(data)
+            data = i[0].split(";")
             date.append(data[1])
             value1.append(data[2])
             value2.append(data[3])

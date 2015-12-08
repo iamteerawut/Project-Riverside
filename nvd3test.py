e2c33622fa63440673c5f6a243f7cc6688513d06
iamteerawut <t.kitbunjerdjarud@gmail.com>
2015-12-08 23:34:10 +0700

    change to lineChart

--- nvd3test.py
+++ nvd3test.py
@@ -1,16 +1,20 @@
-from nvd3 import stackedAreaChart
+from nvd3 import lineChart
 
+# Open File to write the D3 Graph
 output_file = open('test-nvd3.html', 'w')
 
-chart = stackedAreaChart(name='stackedAreaChart', height=400, width=400)
+chart = lineChart(name="lineChart", x_is_date=False, x_axis_format="AM_PM")
 
-xdata = [100, 101, 102, 103, 104, 105, 106,]
-ydata = [6, 11, 12, 7, 11, 10, 11]
+xdata = range(24)
+ydata = [0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 4, 3, 3, 5, 7, 5, 3, 16, 6, 9, 15, 4, 12]
+ydata2 = [9, 8, 11, 8, 3, 7, 10, 8, 6, 6, 9, 6, 5, 4, 3, 10, 0, 6, 3, 1, 0, 0, 0, 1]
 
-extra_serie = {"tooltip": {"y_start": "There is ", "y_end": " min"}}
-chart.add_serie(name="Serie 1", y=ydata, x=xdata, extra=extra_serie)
+extra_serie = {"tooltip": {"y_start": "There are ", "y_end": " calls"}}
+chart.add_serie(y=ydata, x=xdata, name='sine', extra=extra_serie)
+extra_serie = {"tooltip": {"y_start": "", "y_end": " min"}}
+chart.add_serie(y=ydata2, x=xdata, name='cose', extra=extra_serie)
 chart.buildhtml()
 output_file.write(chart.htmlcontent)
 
-
+# close Html file
 output_file.close()

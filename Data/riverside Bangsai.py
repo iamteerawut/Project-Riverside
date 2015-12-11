cf54bf280ba5ce72f2d70afa57a281b97a885b19
nanntch <phiinnyx.00@gmail.com>
2015-12-12 00:03:11 +0700

    updated project ...

--- Data/riverside Bangsai.py
+++ Data/riverside Bangsai.py
@@ -1,24 +1,29 @@
 import csv
-from nvd3 import lineChart
 def project_um():
     with open('Bangsai.csv', encoding="utf-8") as csvfile:
         test = csv.reader(csvfile)
-        list_date = []
-        list_do = []
-        list_ec = []
-        list_temp = []
+        date = []
+        value1 = []
+        value2 = []
+        value3 = []
+        value4 = []
+        value5 = []
         for i in test:
             data = i[0].split(';')
-            list_date.append(data[1])
-            list_do.append(data[2])
-            list_ec.append(data[3])
-            list_temp.append(data[4])
-    list_do = change_value_to_float(list_do)
-    list_ec = change_value_to_float(list_ec)
-    list_temp = change_value_to_float(list_temp)
-    return len(list_do)
-    #created HTML
-    #chart = lineChart(name="lineChart", x_is_date=True, 
+            # print(data)
+            date.append(data[1])
+            value1.append(data[2])
+            value2.append(data[3])
+            value3.append(data[4])
+            value4.append(data[5])
+            value5.append(data[6])
+    lst_val_1 = change_value_to_float(value1)
+    lst_val_2 = change_value_to_float(value2)
+    lst_val_3 = change_value_to_float(value3)
+    lst_val_4 = change_value_to_string(value4)
+    lst_val_5 = change_value_to_string(value5)
+
+    return lst_val_1[0], lst_val_2[0], lst_val_3[0], lst_val_4[0], lst_val_5[0]
 
 def change_value_to_float(valu):
     list_value = []
@@ -31,11 +36,13 @@ def change_value_to_float(valu):
         loop += 1
     return list_value
 
-print(project_um())
-
-
-##เก็บค่าของแต่ละ value ไว้ในลิสต์ของแต่ละค่า
-#แก้ชื่อตัวแปรน่ะ
-#do = ปริมาณออกซิเจนที่ละลายในน้ำ ค่าเป็นเลขหลักเดียวกับทศนิยม เช่น 5.8, 6.4
-#ec = ค่าความนำไฟฟ้า ค่าส่วนใหญ่เป็นเลขสามหลักกับทศนิยม เช่น 148.3, 154.5
-#temp = อุณภูมิน้ำ ค่าส่วนใหญ่เป็นเลขสองหลักกับทศนิยม เช่น 19.5, 33.0
+def change_value_to_string(valu):
+    list_value = []
+    loop = 0
+    for i in valu:
+        value = i[1:len(valu[loop])-1]
+        list_value.append(value)
+        loop += 1
+    return(list_value)
+    
+# print(project_um())
---
+++ Data/แปปนึงยังไม่แน่ใจ/riverside_average.py
@@ -0,0 +1,69 @@
+import riverside_ayuthaya
+import riverside_bangsai
+import riverside_chainat
+import riverside_dowkanong
+import riverside_nakhonsawan
+import riverside_pamok
+import riverside_samlae
+from nvd3 import lineChart
+
+list_date, list_do, list_ec, list_temp, ave_do_ayuthaya, ave_ec_ayuthaya, ave_temp_ayuthaya = riverside_ayuthaya.data_split()
+list_date, list_do, list_ec, list_temp, ave_do_bangsai, ave_ec_bangsai, ave_temp_bangsai = riverside_bangsai.data_split()
+list_date, list_do, list_ec, list_temp, ave_do_chainat, ave_ec_chainat, ave_temp_chainat = riverside_chainat.data_split()
+list_date, list_do, list_ec, list_temp, ave_do_dowkanong, ave_ec_dowkanong, ave_temp_dowkanong = riverside_dowkanong.data_split()
+list_date, list_do, list_ec, list_temp, ave_do_nakhonsawan, ave_ec_nakhonsawan, ave_temp_nakhonsawan = riverside_nakhonsawan.data_split()
+list_date, list_do, list_ec, list_temp, ave_do_pamok, ave_ec_pamok, ave_temp_pamok = riverside_pamok.data_split()
+list_date, list_do, list_ec, list_temp, ave_do_samlae, ave_ec_samlae, ave_temp_samlae = riverside_samlae.data_split()
+#print(ave_do_ayuthaya, ave_do_bangsai, ave_do_chainat, ave_do_dowkanong, ave_do_nakhonsawan, ave_do_pamok, ave_do_samlae)
+all_ave_do = [ave_do_ayuthaya, ave_do_bangsai, ave_do_chainat, ave_do_dowkanong, ave_do_nakhonsawan, ave_do_pamok, ave_do_samlae]
+all_ave_ec = [ave_ec_ayuthaya, ave_ec_bangsai, ave_ec_chainat, ave_ec_dowkanong, ave_ec_nakhonsawan, ave_ec_pamok, ave_ec_samlae]
+all_ave_temp = [ave_temp_ayuthaya, ave_temp_bangsai, ave_temp_chainat, ave_temp_dowkanong, ave_temp_nakhonsawan, ave_temp_pamok, ave_temp_samlae]
+
+#created HTML
+def ave_do_chart():
+    output_file = open('ave_do_chart.html', 'w')
+    chart = lineChart(name="lineChart of average D.O.", width=1244)
+    xdata = range(9)
+    ydata = all_ave_do
+
+    extra_serie = {"tooltip": {"y_start": "There are", "y_end": " calls"}}
+    chart.add_serie(y=ydata, x=xdata, name='D.O.', extra=extra_serie)
+    chart.buildhtml()
+    output_file.write(chart.htmlcontent)
+    # close Html file
+    output_file.close()
+
+ave_do_chart()
+
+
+#created HTML for average E.C.
+def ave_ec_chart():
+    output_file = open('ave_ec_chart.html', 'w')
+    chart = lineChart(name="lineChart of average E.C.", width=1244)
+    xdata = range(9)
+    ydata = all_ave_ec
+
+    extra_serie = {"tooltip": {"y_start": "There are", "y_end": " calls"}}
+    chart.add_serie(y=ydata, x=xdata, name='E.C.', extra=extra_serie)
+    chart.buildhtml()
+    output_file.write(chart.htmlcontent)
+    # close Html file
+    output_file.close()
+
+ave_ec_chart()
+
+#created HTML for average temperature
+def ave_temp_chart():
+    output_file = open('ave_temp_chart.html', 'w')
+    chart = lineChart(name="lineChart of average Temperature", width=1244)
+    xdata = range(9)
+    ydata = all_ave_temp
+
+    extra_serie = {"tooltip": {"y_start": "There are", "y_end": " calls"}}
+    chart.add_serie(y=ydata, x=xdata, name='Temperature', extra=extra_serie)
+    chart.buildhtml()
+    output_file.write(chart.htmlcontent)
+    # close Html file
+    output_file.close()
+
+ave_temp_chart()

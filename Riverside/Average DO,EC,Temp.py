f4be9b0c64d09420cfa757cd26f122bfa6648323
nanntch <phiinnyx.00@gmail.com>
2015-12-13 11:49:28 +0700

    Average แบบ Bar จากฟ้าเล็ก
    เอา line chart ของแนนออก เอา bar ของฟ้าเล็กไปแนน สวยกว่า

--- Riverside/Average DO,EC,Temp.py
+++ Riverside/Average DO,EC,Temp.py
@@ -5,7 +5,7 @@ import River_Dowkanong
 import River_Nakhonsawan
 import River_Pamok
 import River_Samlae
-from nvd3 import lineChart
+from nvd3 import discreteBarChart
 
 list_date, list_do, list_ec, list_temp, ave_do_ayuthaya, ave_ec_ayuthaya, ave_temp_ayuthaya = River_Ayuthaya.data_split()
 list_date, list_do, list_ec, list_temp, ave_do_bangsai, ave_ec_bangsai, ave_temp_bangsai = River_Bangsai.data_split()
@@ -22,11 +22,12 @@ all_ave_temp = [ave_temp_ayuthaya, ave_temp_bangsai, ave_temp_chainat, ave_temp_
 #created HTML
 def ave_do_chart():
     output_file = open('Average-DO.html', 'w')
-    chart = lineChart(name="lineChart of average D.O.", width=1244)
-    xdata = range(9)
+    chart = discreteBarChart(name='discreteBarChart of average D.O.', width=1244)
+
+    xdata = ["Ayuthaya", "Bangsai", "Chainat", "Dowkanong", "Nakhonsawan", "Pamok", "Samlea"]
     ydata = all_ave_do
 
-    extra_serie = {"tooltip": {"y_start": "There are", "y_end": " calls"}}
+    extra_serie = {"tooltip": {"y_start": "", "y_end": "m/L"}}
     chart.add_serie(y=ydata, x=xdata, name='D.O.', extra=extra_serie)
     chart.buildhtml()
     output_file.write(chart.htmlcontent)
@@ -39,11 +40,11 @@ ave_do_chart()
 #created HTML for average E.C.
 def ave_ec_chart():
     output_file = open('Average-EC.html', 'w')
-    chart = lineChart(name="lineChart of average E.C.", width=1244)
-    xdata = range(9)
+    chart = discreteBarChart(name="lineChart of average E.C.", width=1244)
+    xdata = ["Ayuthaya", "Bangsai", "Chainat", "Dowkanong", "Nakhonsawan", "Pamok", "Samlea"]
     ydata = all_ave_ec
 
-    extra_serie = {"tooltip": {"y_start": "There are", "y_end": " calls"}}
+    extra_serie = {"tooltip": {"y_start": "", "y_end": " m"}}
     chart.add_serie(y=ydata, x=xdata, name='E.C.', extra=extra_serie)
     chart.buildhtml()
     output_file.write(chart.htmlcontent)
@@ -55,11 +56,11 @@ ave_ec_chart()
 #created HTML for average temperature
 def ave_temp_chart():
     output_file = open('Average-Temp.html', 'w')
-    chart = lineChart(name="lineChart of average Temperature", width=1244)
-    xdata = range(9)
+    chart = discreteBarChart(name="lineChart of average Temperature", width=1244)
+    xdata = ["Ayuthaya", "Bangsai", "Chainat", "Dowkanong", "Nakhonsawan", "Pamok", "Samlea"]
     ydata = all_ave_temp
 
-    extra_serie = {"tooltip": {"y_start": "There are", "y_end": " calls"}}
+    extra_serie = {"tooltip": {"y_start": "", "y_end": " C"}}
     chart.add_serie(y=ydata, x=xdata, name='Temperature', extra=extra_serie)
     chart.buildhtml()
     output_file.write(chart.htmlcontent)
